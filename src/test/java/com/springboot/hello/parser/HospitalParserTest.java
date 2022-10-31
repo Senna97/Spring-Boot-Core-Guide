57cdd59aeca3b433676b174d4e905ca50ba59dbc
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-10-31 15:41:59 +0900

    assertTrue에서 AssertionFailedError 발생

--- src/test/java/com/springboot/hello/parser/HospitalParserTest.java
+++ src/test/java/com/springboot/hello/parser/HospitalParserTest.java
@@ -3,13 +3,38 @@ package com.springboot.hello.parser;
 import com.springboot.hello.domain.Hospital;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class HospitalParserTest {
     String line1 = "\"1\",\"의원\",\"01_01_02_P\",\"3620000\",\"PHMA119993620020041100004\",\"19990612\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"062-515-2875\",\"\",\"500881\",\"광주광역시 북구 풍향동 565번지 4호 3층\",\"광주광역시 북구 동문대로 24, 3층 (풍향동)\",\"61205\",\"효치과의원\",\"20211115113642\",\"U\",\"2021-11-17 02:40:00.0\",\"치과의원\",\"192630.735112\",\"185314.617632\",\"치과의원\",\"1\",\"0\",\"0\",\"52.29\",\"401\",\"치과\",\"\",\"\",\"\",\"0\",\"0\",\"\",\"\",\"0\",\"\"";
+
+
+    @Autowired
+    ReadLineContext<Hospital> hospitalReadLineContext;
+
+    @Test
+    @DisplayName("10만건 이상의 데이터가 파싱 되는지")
+    void oneHundredThousandRows() throws IOException {
+        // 서버 환경에서 build할 때 문제가 생길 수 있다.
+        // 테스트 코드는 어디에서든지 실행할 수 있게 짜는 것이 목표
+        String fileName = "/Users/jangseohyeon/Downloads/전국 병의원 정보.csv";
+        List<Hospital> hospitalList = hospitalReadLineContext.readByLine(fileName);
+        assertTrue(hospitalList.size() > 1000);
+        assertTrue(hospitalList.size() > 10000);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(hospitalList.get(i).getHospitalName());
+        }
+
+        System.out.printf("파싱된 데이터 개수: %d", hospitalList.size());
+    }
 
     @Test
     @DisplayName("csv 1줄이 Hospital 로 잘 만들어지는지 Test")
@@ -20,8 +45,8 @@ class HospitalParserTest {
 
         assertEquals(1, hospital.getId()); // col: 0
         assertEquals("의원", hospital.getOpenServiceName()); // col: 1
-        assertEquals(3620000,hospital.getOpenLocalGovernmentCode()); // col: 3
-        assertEquals("PHMA119993620020041100004",hospital.getManagementNumber()); // col:4
+        assertEquals(3620000, hospital.getOpenLocalGovernmentCode()); // col: 3
+        assertEquals("PHMA119993620020041100004", hospital.getManagementNumber()); // col:4
         assertEquals(LocalDateTime.of(1999, 6, 12, 0, 0, 0), hospital.getLicenseDate()); //19990612, col: 5
         assertEquals(1, hospital.getBusinessStatus()); // col: 7
         assertEquals(13, hospital.getBusinessStatusCode()); // col: 9
